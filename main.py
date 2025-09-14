fde5c290b04f4905f04d97f00e8c691b5c73d1ce
Marvelous Ojediran <ravenclawrockstar@gmail.com>
2025-09-14 23:25:03 +0000

    code

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, World");
+print("Hello, Welcome to the Marvelous ChatBot!");
+
+collect_name = str(input("What is your name?: "));
+collect_age = int(input("What is your age?: ")); 
+
+print("Hello"", "+collect_name+"!","How can I help you?" );  
+
+
+print("1. Placeholder 1");
+print("2. Placeholder 2"); 
+print("3. Placeholder 3");
+print("4. Placeholder 4");
+
+user_choice = int(input("Pick an option from the menu for help: "));
+
+if user_choice == 1:
+    print("Placeholder 1 selected.")
+elif user_choice == 2:
+    print("Placeholder 2 selected.")
+elif user_choice == 3:
+    print("Placeholder 3 selected.")
+elif user_choice == 4:
+    print("Placeholder 4 selected.")
+else:
+    print("Invalid choice")
+                        
+
+
